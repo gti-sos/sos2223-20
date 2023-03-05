2aa2877d6b46c578893c7efd3922f140bc80ecbf
Miguel Angel Salinas <masm.htc@gmail.com>
2023-03-05 20:17:13 +0100

    nothing

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get("/cool", (req,res) => {
     res.send(cool());
     console.log("New request");
 });
+app.get("/cool", (req,res) => {
+  res.send(cool());
+  console.log("New request");
+});
 
 app.listen(port,() =>{
   console.log(`Server ready in port ${port}`);
