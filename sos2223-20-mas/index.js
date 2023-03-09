f350c134abf52a5f8c342ac976e930bfd6bb3217
Miguel Angel Salinas <masm.htc@gmail.com>
2023-03-09 15:07:21 +0100

    minimal changes

--- sos2223-20-mas/index.js
+++ sos2223-20-mas/index.js
@@ -3,23 +3,26 @@ var express = require("express");
 var cool = require("cool-ascii-faces");
 var port = process.env.PORT || 12345;
 var app = express();
-var useMAS = require("./samples/MAS");
-var useLMP = require("./samples/LMP");
-var useCGM = require("./samples/CGM");
 
-const bodyParser = require("body-parser");
-app.use(bodyParser.json());
+var contacts = [
+  { name:"Nombre1",
+    phone: "Number phone"
+  },
+  {
+    name: "Nombre 2",
+    phone: "phone2"
+  }
+];
 
-app.get("/cool", (req,res) => {
-    res.send(cool());
-    console.log("New request");
+const BASE_API_URL = "/api/v1";
+
+//res.json devuelve en json lo que le mandemos.
+app.get(BASE_API_URL+"/contacts", (req,res) => {
+  res.json(contacts);
+  console.log("New request");
 });
-
-app.get("/samples/MAS", useMAS);
-app.get("/samples/LMP", useLMP);
-app.get("/samples/CGM", useCGM);
-
 
 app.listen(port,() =>{
   console.log(`Servidor corriendo en el puerto: ${port}`);
 });
+
