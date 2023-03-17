e9dffb7e1325c0d6f769a54d4660b37be4949778
Carmen García Muñoz <cargarmun5@alum.us.es>
2023-03-17 12:07:59 +0100

    Carmen index L06

--- samples/CGM.js
+++ samples/CGM.js
@@ -1,22 +1,140 @@
 const datos = [
-    [new Date("13-07-16"),"CRTS CORDOBA",8, 6, 1, 0, 0,"SPC",13001,"CTS Córdoba"],
-    [new Date("13-07-16"),"CRTS CORDOBA",5,4,1,0,0,"SPC",13001,"CTS Córdoba"],
-    [new Date("14-07-16"),"ADAMUZ",65,57,0,0,1,"SP",13001,"CTS Córdoba"],
-    [new Date("14-07-16"),"MORILES",67,59,2,0,3,"SP",13001,"CTS Córdoba"],
-    [new Date("14-07-16"),"CESPEDES",20,17,0,0,0,"SP",13001,"CTS Córdoba"],
-    [new Date("15-07-16"),"CRTS CORDOBA",14,10,1,0,4,"SPC",13001,"CTS Córdoba"],
-    [new Date("15-07-16"),"TORRECAMPO",37,35,0,0,2,"SP",13001,"CTS Córdoba"],
-    [new Date("15-07-16"),"CRTS CORDOBA",16,12,1,0,2,"SPC",13001,"CTS Córdoba"],
-    [new Date("13-07-16"),"POZOBLANCO",113,91,4,0,12,"SP",13001,"CTS Córdoba"],
-    [new Date("14-07-16"),"CRTS CORDOBA",34,28,0,0,4,"SPC",13001,"CTS Córdoba"],
+  {
+    date:"13-07-16",
+    place:"CRTS CORDOBA",
+    dnt_people:8,
+    dnt_blood:6,
+    dnt_plasme:1,
+    dnt_datef:0,
+    dnt_new:0,
+    extraction:"SPC",
+    idcenter:13001,
+    center:"CTS Córdoba"
+  },
+  {
+    date:"13-07-16",
+    place:"CRTS CORDOBA",
+    dnt_people:5,
+    dnt_blood:4,
+    dnt_plasme:1,
+    dnt_datef:0,
+    dnt_new:0,
+    extraction:"SPC",
+    idcenter:13001,
+    center:"CTS Córdoba"
+  },
+  {
+    date:"14-07-16",
+    place:"ADAMUZ",
+    dnt_people:65,
+    dnt_blood:57,
+    dnt_plasme:0,
+    dnt_datef:0,
+    dnt_new:1,
+    extraction:"SP",
+    idcenter:13001,
+    center:"CTS Córdoba"
+  },
+  {
+    date:"14-07-16",
+    place:"MORILES",
+    dnt_people:67,
+    dnt_blood:59,
+    dnt_plasme:2,
+    dnt_datef:0,
+    dnt_new:3,
+    extraction:"SP",
+    idcenter:13001,
+    center:"CTS Córdoba"
+  },
+  {
+    date:"14-07-16",
+    place:"CESPEDES",
+    dnt_people:20,
+    dnt_blood:17,
+    dnt_plasme:0,
+    dnt_datef:0,
+    dnt_new:0,
+    extraction:"SP",
+    idcenter:13001,
+    center:"CTS Córdoba"
+  },
+  {
+    date:"15-07-16",
+    place:"CRTS CORDOBA",
+    dnt_people:14,
+    dnt_blood:10,
+    dnt_plasme:1,
+    dnt_datef:0,
+    dnt_new:4,
+    extraction:"SPC",
+    idcenter:13001,
+    center:"CTS Córdoba"
+  },
+  {
+    date:"15-07-16",
+    place:"TORRECAMPO",
+    dnt_people:37,
+    dnt_blood:35,
+    dnt_plasme:0,
+    dnt_datef:0,
+    dnt_new:2,
+    extraction:"SP",
+    idcenter:13001,
+    center:"CTS Córdoba"
+  },
+  {
+    date:"15-07-16",
+    place:"CRTS CORDOBA",
+    dnt_people:16,
+    dnt_blood:12,
+    dnt_plasme:1,
+    dnt_datef:0,
+    dnt_new:2,
+    extraction:"SPC",
+    idcenter:13001,
+    center:"CTS Córdoba"
+  },
+  {
+    date:"13-07-16",
+    place:"POZOBLANCO",
+    dnt_people:113,
+    dnt_blood:91,
+    dnt_plasme:4,
+    dnt_datef:0,
+    dnt_new:12,
+    extraction:"SP",
+    idcenter:13001,
+    center:"CTS Córdoba"
+  },
+  {
+    date:"14-07-16",
+    place:"CRTS CORDOBA",
+    dnt_people:34,
+    dnt_blood:28,
+    dnt_plasme:0,
+    dnt_datef:0,
+    dnt_new:4,
+    extraction:"SPC",
+    idcenter:13001,
+    center:"CTS Córdoba"
+  }
 ];
 
-const filtrado = datos.filter((n) => n[1] === "CRTS CORDOBA").map((n) => { return n[2] });
-const resultado = filtrado.reduce((a, b) => a + b, 0) / filtrado.length;
-
-function handleMiembro1(req, res) {
-    console.log(`La media es: ${resultado}`);
-    res.send(`La media  es: ${resultado}`);
-  }
+function resultado(datos){
+  var valores = datos.filter(p => p.place == "CRTS CORDOBA");
   
-module.exports = handleMiembro1;
+  var valores_db = [];
+  valores.forEach(valor => {valores_db.push(valor.dnt_blood)});
+  suma_total = 0;
+  valores_db.forEach(num => {suma_total+= num});
+  var res ="La media es: "+ suma_total/valores_db.length;
+  return res;
+}
+
+// function handleMiembro1(req, res) {
+//   res.send(`La media  es: ${resultado(datos)}`);
+// }
+
+module.exports.datos = datos;
+module.exports.resultado = resultado;
