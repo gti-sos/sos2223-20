fea1b2a1875661d2c34c4773dd5b380312137e0d
Miguel Angel Salinas <masm.htc@gmail.com>
2023-03-19 16:03:30 +0100

    minor changes

--- index.js
+++ index.js
@@ -1,27 +1,16 @@
 
 var express = require("express");
-var cool = require("cool-ascii-faces");
 var port = process.env.PORT || 12345;
 var app = express();
-var useMAS = require("./samples/MAS");
-var useLMP = require("./samples/LMP");
-var useCGM = require("./samples/CGM");
 const campingsFilePath = 'ddbb/camping-andalusia.json';
 const immovablesFilePath = 'ddbb/immovables.json';
 const fs = require('fs');
 const BASE_API_URL = "/api/v1";
 const bodyParser = require("body-parser");
+
 app.use(bodyParser.json());
+app.use("/", express.static("./public"));
 
-
-app.get("/cool", (req,res) => {
-    res.send(cool());
-    console.log("New request");
-});
-//L05
-app.get("/samples/MAS", useMAS);
-app.get("/samples/LMP", useLMP);
-//app.get("/samples/CGM", useCGM);
 
 //L06 MAS______________________________________________________________________________________
 
