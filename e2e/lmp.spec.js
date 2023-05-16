1bdfc082b3417ecc5863456eb8b0cce9201f5943
Lukmare <lorenzo_mp8@hotmail.com>
2023-05-17 01:47:38 +0200

    Update lmp.spec.js

--- e2e/lmp.spec.js
+++ e2e/lmp.spec.js
@@ -3,5 +3,5 @@ import { test, expect } from '@playwright/test';
 
 test('immovables page has right title', async ({ page }) => {
     await page.goto('https://sos2223-20.ew.r.appspot.com/');
-    expect(await page.title()).toBe("/SOS2223-20/");
+    expect(await page.title()).toBe("SOS2223-20");
 });
