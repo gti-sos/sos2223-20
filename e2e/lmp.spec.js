f1da4a5087f8757cd646acd1c83e59ed73df180e
Lukmare <lorenzo_mp8@hotmail.com>
2023-05-16 23:06:18 +0200

    Update lmp.spec.js

--- e2e/lmp.spec.js
+++ e2e/lmp.spec.js
@@ -2,13 +2,13 @@
 import { test, expect } from '@playwright/test';
 
 test('immovables page has right title', async ({ page }) => {
-    await page.goto('http://localhost:12345/api/v2/immovables');
-    expect(await page.title()).toBe("Immuebles");
+    await page.goto('https://sos2223-20.ew.r.appspot.com/api/v2/immovables');
+    expect(await page.title()).toBe("/Immuebles/");
 });
 
-test('market-prices-stats create data works', async ({ page }) => {
-    await page.goto('http://localhost:12345/api/v2/immovables');
+test('immovables create data works', async ({ page }) => {
+    await page.goto('https://sos2223-20.ew.r.appspot.com/api/v2/immovables');
     await page.getByText('Cargar Recursos').click();
-    await page.waitForSelector('table');
+    await page.waitForSelector('Table');
     await expect((await page.$$('table tr')).length).toBeGreaterThan(1);
 });
