b4678b0783d2fee97a17ff78e6960a7e328a6bfd
Lukmare <lorenzo_mp8@hotmail.com>
2023-05-16 22:10:52 +0200

    Update lmp.spec.js

--- e2e/lmp.spec.js
+++ e2e/lmp.spec.js
@@ -1,13 +1,6 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-    await page.goto('https://sos2223-20.ew.r.appspot.com/');
-  
-    // Expect a title "to contain" a substring.
-    await expect(page).toHaveTitle(/SOS2223-20/);
-  });
-
   test('has title', async ({ page }) => {
     await page.goto('https://sos2223-20.ew.r.appspot.com/api/v2/immovables');
   
