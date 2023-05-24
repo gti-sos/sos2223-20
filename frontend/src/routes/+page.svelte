31f8f59332c5bd1196c4dbaefe52caf7f73b8f4e
Miguel Angel Salinas <masm.htc@gmail.com>
2023-05-24 19:59:22 +0200

    quitamos a carmen

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,63 +1,162 @@
 <svelte:head>
-    <title> SOS2223-20 </title>
+    <title>SOS2223-20</title>
 </svelte:head>
 <main>
     <body>
         <h1>SOS2223-20</h1>
         <ul dir="auto">
-            <li><strong>Equipo</strong>
-            <ul dir="auto">
-            <li><a href="https://github.com/Lukmare">Lorenzo Morales Palacios (Fuente: immovables)</a></li>
-            <li><a href="https://github.com/salinosaso">Miguel Ángel Salinas(Fuente: andalusian-campings) </a></li>
-            <li><a href="https://github.com/cargarmun">Carmen García Muñoz (Fuente: blood-donations) </a></li>
-            </ul>
-            </li>
-            <li><strong>Descripción del Proyecto</strong>: Analizando los inmuebles con respescto a las viviendas turísticas y de Camping</li>
-            <li><strong>Repositorio</strong>: <a href="https://github.com/gti-sos/SOS2223-20">gti-sos/SOS2223-20</a></li>
-            <li><strong>URL</strong>: <a href="http://sos2223-20.appspot.com/">http://sos2223-20.appspot.com/ </a></li>
-            <li><strong>APIs v1</strong>:
-            <ul dir="auto">
-            <li><a href="http://sos2223-20.appspot.com/api/v1/immovables/docs" rel="nofollow">http://sos2223-20.appspot.com/api/v1/immovables/docs</a> (desarrollado por <a href="https://github.com/Lukmare">Lorenzo Morales Palacios</a>)</li>
-            <li><a href="http://sos2223-20.appspot.com/api/v1/andalusian-campings/docs" rel="nofollow">http://sos2223-20.appspot.com/api/v1/andalusian-campings/docs</a> (desarrollado por <a href="https://github.com/salinosaso">Miguel Ángel Salinas</a>)</li>
-            <li><a href="http://sos2223-20.appspot.com/api/v1/blood-donations" rel="nofollow">http://sos2223-20.appspot.com/api/v1/blood-donations/</a> (desarrollado por <a href="https://github.com/cargarmun">Carmen García Muñoz</a>)</li>
-            
-            <li><strong>APIs v2</strong>:
+            <li>
+                <strong>Equipo</strong>
                 <ul dir="auto">
-                <li><a href="http://sos2223-20.appspot.com/api/v2/immovables/docs" rel="nofollow">http://sos2223-20.appspot.com/api/v2/immovables/docs</a> (desarrollado por <a href="https://github.com/Lukmare">Lorenzo Morales Palacios</a>)</li>
-                <li><a href="https://documenter.getpostman.com/view/26063155/2s93XyU3Xd" rel="nofollow">http://sos2223-20.appspot.com/api/v2/andalusian-campings/docs</a> (desarrollado por <a href="https://github.com/salinosaso">Miguel Ángel Salinas</a>)</li>
-                <li><a href="http://sos2223-20.appspot.com/api/v2/blood-donations/" rel="nofollow">http://sos2223-20.appspot.com/api/v2/blood-donations/</a> (desarrollado por <a href="https://github.com/cargarmun">Carmen García Muñoz</a>)</li>
-                
-            <li><strong>Documentación de Postman v1</strong>
-                <ul dir="auto">
-                <li><a href="https://documenter.getpostman.com/view/26062489/2s93K1oysD">Doc Inmuebles </a></li>
-                <li><a href="https://documenter.getpostman.com/view/26063155/2s93K1oeqs">Doc Campings Andalucia</a></li>
+                    <li>
+                        <a href="https://github.com/Lukmare"
+                            >Lorenzo Morales Palacios (Fuente: immovables)</a
+                        >
+                    </li>
+                    <li>
+                        <a href="https://github.com/salinosaso"
+                            >Miguel Ángel Salinas(Fuente: andalusian-campings)
+                        </a>
+                    </li>
                 </ul>
-            </li> 
-                
-                <li><strong>Documentación de Postman v2</strong>
-                    <ul dir="auto">
-                    <li><a href="https://documenter.getpostman.com/view/26062489/2s93XsZn3G">Doc Inmuebles </a></li>
-                    <li><a href="https://documenter.getpostman.com/view/26063155/2s93XyU3Xd">Doc Campings Andalucia</a></li>
-                    </ul>
-                </li> 
-                <li><strong>Documentación de Postman v3</strong>
-                    <ul dir="auto">
-                    <li><a href="https://documenter.getpostman.com/view/26062489/2s93kz4PpH">Doc Inmuebles </a></li>
-                    <li><a href="https://documenter.getpostman.com/view/26063155/2s93m611ua">Doc Campings </a></li>
-                    
-                    </ul>
-                </li> 
-                
-                <li><strong>Frontend</strong>
-                    <ul><li><a href="http://sos2223-20.appspot.com/api/v2/blood-donations">Donaciones Sangre</a></li></ul>
-                    <a href="/api/v2/immovables">Inmuebles</a>
-                    <a href="/api/v2/andalusian-campings">Campings Andalucia</a>
-                </li>
-        </ul>
-        
-            </ul>
-    </body>
+            </li>
+            <li>
+                <strong>Descripción del Proyecto</strong>: Analizando los
+                inmuebles con respescto a las viviendas turísticas y de Camping
+            </li>
+            <li>
+                <strong>Repositorio</strong>:
+                <a href="https://github.com/gti-sos/SOS2223-20"
+                    >gti-sos/SOS2223-20</a
+                >
+            </li>
+            <li>
+                <strong>URL</strong>:
+                <a href="http://sos2223-20.appspot.com/"
+                    >http://sos2223-20.appspot.com/
+                </a>
+            </li>
+            <li>
+                <strong>APIs v1</strong>:
+                <ul dir="auto">
+                    <li>
+                        <a
+                            href="http://sos2223-20.appspot.com/api/v1/immovables/docs"
+                            rel="nofollow"
+                            >http://sos2223-20.appspot.com/api/v1/immovables/docs</a
+                        >
+                        (desarrollado por
+                        <a href="https://github.com/Lukmare"
+                            >Lorenzo Morales Palacios</a
+                        >)
+                    </li>
+                    <li>
+                        <a
+                            href="http://sos2223-20.appspot.com/api/v1/andalusian-campings/docs"
+                            rel="nofollow"
+                            >http://sos2223-20.appspot.com/api/v1/andalusian-campings/docs</a
+                        >
+                        (desarrollado por
+                        <a href="https://github.com/salinosaso"
+                            >Miguel Ángel Salinas</a
+                        >)
+                    </li>
+
+                    <li>
+                        <strong>APIs v2</strong>:
+                        <ul dir="auto">
+                            <li>
+                                <a
+                                    href="http://sos2223-20.appspot.com/api/v2/immovables/docs"
+                                    rel="nofollow"
+                                    >http://sos2223-20.appspot.com/api/v2/immovables/docs</a
+                                >
+                                (desarrollado por
+                                <a href="https://github.com/Lukmare"
+                                    >Lorenzo Morales Palacios</a
+                                >)
+                            </li>
+                            <li>
+                                <a
+                                    href="https://documenter.getpostman.com/view/26063155/2s93XyU3Xd"
+                                    rel="nofollow"
+                                    >http://sos2223-20.appspot.com/api/v2/andalusian-campings/docs</a
+                                >
+                                (desarrollado por
+                                <a href="https://github.com/salinosaso"
+                                    >Miguel Ángel Salinas</a
+                                >)
+                            </li>
+
+                            <li>
+                                <strong>Documentación de Postman v1</strong>
+                                <ul dir="auto">
+                                    <li>
+                                        <a
+                                            href="https://documenter.getpostman.com/view/26062489/2s93K1oysD"
+                                            >Doc Inmuebles
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a
+                                            href="https://documenter.getpostman.com/view/26063155/2s93K1oeqs"
+                                            >Doc Campings Andalucia</a
+                                        >
+                                    </li>
+                                </ul>
+                            </li>
+
+                            <li>
+                                <strong>Documentación de Postman v2</strong>
+                                <ul dir="auto">
+                                    <li>
+                                        <a
+                                            href="https://documenter.getpostman.com/view/26062489/2s93XsZn3G"
+                                            >Doc Inmuebles
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a
+                                            href="https://documenter.getpostman.com/view/26063155/2s93XyU3Xd"
+                                            >Doc Campings Andalucia</a
+                                        >
+                                    </li>
+                                </ul>
+                            </li>
+                            <li>
+                                <strong>Documentación de Postman v3</strong>
+                                <ul dir="auto">
+                                    <li>
+                                        <a
+                                            href="https://documenter.getpostman.com/view/26062489/2s93kz4PpH"
+                                            >Doc Inmuebles
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a
+                                            href="https://documenter.getpostman.com/view/26063155/2s93m611ua"
+                                            >Doc Campings
+                                        </a>
+                                    </li>
+                                </ul>
+                            </li>
+
+                            <li>
+                                <strong>Frontend</strong>
+                                <a href="/api/v2/immovables">Inmuebles</a>
+                                <br />
+                                <a href="/api/v2/andalusian-campings"
+                                    >Campings Andalucia</a
+                                >
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+        </ul></body
+    >
 </main>
+
 <style>
     main {
         display: flex;
@@ -66,7 +165,7 @@
         height: 100vh;
         font-family: Arial, sans-serif;
     }
-    
+
     body {
         width: 90%;
         max-width: 800px;
@@ -75,35 +174,34 @@
         border-radius: 5px;
         box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
     }
-    
+
     h1 {
         font-size: 2em;
         text-align: center;
         margin-bottom: 30px;
     }
-    
+
     ul {
         list-style-type: none;
         margin: 0;
         padding: 0;
     }
-    
+
     li {
         margin-bottom: 10px;
     }
-    
+
     a {
         color: #0077cc;
         text-decoration: none;
         transition: color 0.2s;
     }
-    
+
     a:hover {
         color: #005ea1;
     }
-    
+
     strong {
         font-weight: bold;
     }
 </style>
-
