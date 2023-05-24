0fe2499667b2634b79faadbb6f3db025d89f6b87
Miguel Angel Salinas <masm.htc@gmail.com>
2023-05-24 19:55:51 +0200

    postmanv3

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -31,7 +31,6 @@
                 <ul dir="auto">
                 <li><a href="https://documenter.getpostman.com/view/26062489/2s93K1oysD">Doc Inmuebles </a></li>
                 <li><a href="https://documenter.getpostman.com/view/26063155/2s93K1oeqs">Doc Campings Andalucia</a></li>
-                <li><a href="http://sos2223-20.appspot.com/api/v1/blood-donations/docs">Doc Donaciones Sangre</a></li>
                 </ul>
             </li> 
                 
@@ -39,12 +38,13 @@
                     <ul dir="auto">
                     <li><a href="https://documenter.getpostman.com/view/26062489/2s93XsZn3G">Doc Inmuebles </a></li>
                     <li><a href="https://documenter.getpostman.com/view/26063155/2s93XyU3Xd">Doc Campings Andalucia</a></li>
-                    <li><a href="http://sos2223-20.appspot.com/api/v2/blood-donations/docs">Doc Donaciones Sangre</a></li>
                     </ul>
                 </li> 
                 <li><strong>Documentación de Postman v3</strong>
                     <ul dir="auto">
                     <li><a href="https://documenter.getpostman.com/view/26062489/2s93kz4PpH">Doc Inmuebles </a></li>
+                    <li><a href="https://documenter.getpostman.com/view/26063155/2s93m611ua">Doc Campings </a></li>
+                    
                     </ul>
                 </li> 
                 
