79820e3708723b7778041f7814693cdffc68a473
Miguel Angel Salinas <masm.htc@gmail.com>
2023-05-25 14:32:59 +0200

    semi final gchart

--- frontend/src/routes/integrations/mas/api-andalusian-campings/google_chart/+page.svelte
+++ frontend/src/routes/integrations/mas/api-andalusian-campings/google_chart/+page.svelte
@@ -1,203 +1,189 @@
 <script>
-    //@ts-nocheck
+     //@ts-nocheck
     import { onMount } from "svelte";
-  
+    //let API_externaSOS = "/api/proxy-mas/?url=https://sos2223-20.ew.r.appspot.com/api/v2/immovables";
+    
     let API = "https://sos2223-20.ew.r.appspot.com/api/v3/campings";
     let data = [];
-  
+    console.log(data);
+
     async function getData() {
-      const response = await fetch(API);
-      data = await response.json();
+        const response = await fetch(API);
+        data = await response.json();
+        console.log(data);
     }
-  
+
     function getDataAlmeria() {
-      const province = "ALMERÍA";
-      let sumCampingPlaces = 0;
-      data.forEach((item) => {
-        const itemProvince = item.state;
-  
-        if (itemProvince === province) {
-          sumCampingPlaces += item.camping_places;
-        }
-      });
-      return sumCampingPlaces;
+        const province = "ALMERÍA";
+        let sumCampingPlaces = 0;
+        data.forEach((item) => {
+            const itemProvince = item.state;
+
+            if (itemProvince === province) {
+                sumCampingPlaces += item.camping_places;
+            }
+        });
+        console.log(sumCampingPlaces);
+        return sumCampingPlaces;
     }
-  
+
     function getDataMalaga() {
-      const province = "MÁLAGA";
-      let sumCampingPlaces = 0;
-      data.forEach((item) => {
-        const itemProvince = item.state;
-  
-        if (itemProvince === province) {
-          sumCampingPlaces += item.camping_places;
-        }
-      });
-      return sumCampingPlaces;
+        const province = "MÁLAGA";
+        let sumCampingPlaces = 0;
+        data.forEach((item) => {
+            const itemProvince = item.state;
+
+            if (itemProvince === province) {
+                sumCampingPlaces += item.camping_places;
+            }
+        });
+        console.log(sumCampingPlaces);
+        return sumCampingPlaces;
     }
-  
+
     function getDataSevilla() {
-      const province = "SEVILLA";
-      let sumCampingPlaces = 0;
-      data.forEach((item) => {
-        const itemProvince = item.state;
-  
-        if (itemProvince === province) {
-          sumCampingPlaces += item.camping_places;
-        }
-      });
-      return sumCampingPlaces;
+        const province = "SEVILLA";
+        let sumCampingPlaces = 0;
+        data.forEach((item) => {
+            const itemProvince = item.state;
+
+            if (itemProvince === province) {
+                sumCampingPlaces += item.camping_places;
+            }
+        });
+        console.log(sumCampingPlaces);
+        return sumCampingPlaces;
     }
-  
+
     function getDataCadiz() {
-      const province = "CÁDIZ";
-      let sumCampingPlaces = 0;
-      data.forEach((item) => {
-        const itemProvince = item.state;
-  
-        if (itemProvince === province) {
-          sumCampingPlaces += item.camping_places;
-        }
-      });
-      return sumCampingPlaces;
+        const province = "CÁDIZ";
+        let sumCampingPlaces = 0;
+        data.forEach((item) => {
+            const itemProvince = item.state;
+
+            if (itemProvince === province) {
+                sumCampingPlaces += item.camping_places;
+            }
+        });
+        console.log(sumCampingPlaces);
+        return sumCampingPlaces;
     }
-  
+
     function getDataCordoba() {
-      const province = "CÓRDOBA";
-      let sumCampingPlaces = 0;
-      data.forEach((item) => {
-        const itemProvince = item.state;
-  
-        if (itemProvince === province) {
-          sumCampingPlaces += item.camping_places;
-        }
-      });
-      return sumCampingPlaces;
+        const province = "CÓRDOBA";
+        let sumCampingPlaces = 0;
+        data.forEach((item) => {
+            const itemProvince = item.state;
+
+            if (itemProvince === province) {
+                sumCampingPlaces += item.camping_places;
+            }
+        });
+        console.log(sumCampingPlaces);
+        return sumCampingPlaces;
     }
-  
+
     function getDataGranada() {
-      const province = "GRANADA";
-      let sumCampingPlaces = 0;
-      data.forEach((item) => {
-        const itemProvince = item.state;
-  
-        if (itemProvince === province) {
-          sumCampingPlaces += item.camping_places;
-        }
-      });
-      return sumCampingPlaces;
+        const province = "GRANADA";
+        let sumCampingPlaces = 0;
+        data.forEach((item) => {
+            const itemProvince = item.state;
+
+            if (itemProvince === province) {
+                sumCampingPlaces += item.camping_places;
+            }
+        });
+        console.log(sumCampingPlaces);
+        return sumCampingPlaces;
     }
-  
+
     function getDataHuelva() {
-      const province = "HUELVA";
-      let sumCampingPlaces = 0;
-      data.forEach((item) => {
-        const itemProvince = item.state;
-  
-        if (itemProvince === province) {
-          sumCampingPlaces += item.camping_places;
-        }
-      });
-      return sumCampingPlaces;
+        const province = "HUELVA";
+        let sumCampingPlaces = 0;
+        data.forEach((item) => {
+            const itemProvince = item.state;
+
+            if (itemProvince === province) {
+                sumCampingPlaces += item.camping_places;
+            }
+        });
+        console.log(sumCampingPlaces);
+        return sumCampingPlaces;
     }
-  
+
     function getDataJaen() {
-      const province = "JAÉN";
-      let sumCampingPlaces = 0;
-      data.forEach((item) => {
-        const itemProvince = item.state;
-  
-        if (itemProvince === province) {
-          sumCampingPlaces += item.camping_places;
-        }
-      });
-      return sumCampingPlaces;
+        const province = "JAÉN";
+        let sumCampingPlaces = 0;
+        data.forEach((item) => {
+            const itemProvince = item.state;
+
+            if (itemProvince === province) {
+                sumCampingPlaces += item.camping_places;
+            }
+        });
+        console.log(sumCampingPlaces);
+        return sumCampingPlaces;
     }
-  
+
     onMount(async () => {
-      await Promise.all([getData()]);
-      google.charts.load("current", { packages: ["corechart", "table"] });
-      google.charts.setOnLoadCallback(drawCharts);
-  
-      function drawCharts() {
-        var pieData = google.visualization.arrayToDataTable([
-          ["Provincia", "Camping Places"],
-          ["Almería", getDataAlmeria()],
-          ["Cádiz", getDataCadiz()],
-          ["Córdoba", getDataCordoba()],
-          ["Granada", getDataGranada()],
-          ["Jaén", getDataJaen()],
-          ["Huelva", getDataHuelva()],
-          ["Málaga", getDataMalaga()],
-          ["Sevilla", getDataSevilla()],
-        ]);
-  
-        var pieOptions = {
-          title: "Campings in Andalucia (Pie Chart)",
-          is3D: true,
-        };
-  
-        var columnData = google.visualization.arrayToDataTable([
-          ["Provincia", "Plazas de Camping", { role: "style" }],
-          ["Almería", getDataAlmeria(), "gold"],
-          ["Cádiz", getDataCadiz(), "silver"],
-          ["Córdoba", getDataCordoba(), "red"],
-          ["Granada", getDataGranada(), "blue"],
-          ["Jaén", getDataJaen(), "yellow"],
-          ["Huelva", getDataHuelva(), "green"],
-          ["Málaga", getDataMalaga(), "orange"],
-          ["Sevilla", getDataSevilla(), "purple"],
-        ]);
-  
-        var columnOptions = {
-          title: "Plazas de Camping por Provincias (Column Chart)",
-          width: 1600,
-          height: 800,
-          bar: { groupWidth: "95%" },
-          legend: { position: "none" },
-        };
-  
-        var tableData = google.visualization.arrayToDataTable([
-          ["State", "Camping Places", "Start Date"],
-          ...data
-            .sort((a, b) => (a.name > b.name ? 1 : -1))
-            .map((item) => [
-              item.state,
-              item.camping_places,
-              item.start_date,
-            ]),
-        ]);
-  
-        var tableOptions = {
-          title: "Campings Data",
-          showRowNumber: true,
-          sort: "enable",
-          sortColumn: 0,
-        };
-  
-        var pieChart = new google.visualization.PieChart(
-          document.getElementById("piechart_3d")
-        );
-        pieChart.draw(pieData, pieOptions);
-  
-        var columnChart = new google.visualization.ColumnChart(
-          document.getElementById("columnchart_values")
-        );
-        columnChart.draw(columnData, columnOptions);
-  
-        var tableChart = new google.visualization.Table(
-          document.getElementById("tablechart_values")
-        );
-        tableChart.draw(tableData, tableOptions);
-      }
+        await Promise.all([getData()]);
+        google.charts.load("current", { packages: ["corechart"] });
+        google.charts.setOnLoadCallback(drawChart);
+
+        function drawChart() {
+            var pieData = google.visualization.arrayToDataTable([
+                ["Provincia", "Camping Places"],
+                ["Almería", getDataAlmeria()],
+                ["Cádiz", getDataCadiz()],
+                ["Córdoba", getDataCordoba()],
+                ["Granada", getDataGranada()],
+                ["Jaén", getDataJaen()],
+                ["Huelva", getDataHuelva()],
+                ["Málaga", getDataMalaga()],
+                ["Sevilla", getDataSevilla()],
+            ]);
+            
+            var pieOptions = {
+                title: "Campings in Andalucia (Pie Chart)",
+                is3D: true,
+            };
+
+            var columnData = google.visualization.arrayToDataTable([
+                ["Provincia", "Plazas de Camping", { role: "style" }],
+                ["Almería", getDataAlmeria(), "gold"],
+                ["Cádiz", getDataCadiz(), "silver"],
+                ["Córdoba", getDataCordoba(),"red"],
+                ["Granada", getDataGranada(),"blue"],
+                ["Jaén", getDataJaen(),"yellow"],
+                ["Huelva", getDataHuelva(),"green"],
+                ["Málaga", getDataMalaga(),"orange"],
+                ["Sevilla", getDataSevilla(),"purple"],
+            ]);
+            
+            var columnOptions = {
+                title: "Plazas de Camping por Provincias (Column Chart)",
+                width: 1600,
+                height: 800,
+                bar: { groupWidth: "95%" },
+                legend: { position: "none" },
+            };
+
+            var pieChart = new google.visualization.PieChart(
+                document.getElementById("piechart_3d")
+            );
+            pieChart.draw(pieData, pieOptions);
+
+            var columnChart = new google.visualization.ColumnChart(
+                document.getElementById("columnchart_values")
+            );
+            columnChart.draw(columnData, columnOptions);
+        }
     });
-  </script>
-  
-  <svelte:head>
+</script>
+
+<svelte:head>
     <script src="https://www.gstatic.com/charts/loader.js"></script>
-  </svelte:head>
-  
-  <div id="piechart_3d" style="width: 1500px; height: 600px;"></div>
-  <div id="columnchart_values" style="width: 1500px; height: 900px;"></div>
-  <div id="tablechart_values" style="width: 100%; height: 100%;"></div>
-  
+</svelte:head>
+
+<div id="piechart_3d" style="width: 1500px; height: 600px;" />
+<div id="columnchart_values" style="width: 1500px; height: 900px;" />
