e78d7e343affccbf519c3288672ed1664b1088fa
Miguel Angel Salinas <masm.htc@gmail.com>
2023-05-25 12:33:29 +0200

    create about y analytics

--- frontend/src/routes/Header.svelte
+++ frontend/src/routes/Header.svelte
@@ -2,8 +2,10 @@
   <a href="/">Inicio</a>
   <a href="/api/v2/immovables">Inmuebles</a>
   <a href="/api/v2/andalusian-campings">Campings Andalucia</a>
-  <a href="/api/v2/blood-donations">Donaciones Sangre</a>
+  
   <a href="/integrations">Integraciones</a>
+  <a href="/analytics">Integración Conjunta</a>
+  <a href="/about">Vídeo</a>
 </nav>
 
 <style>
